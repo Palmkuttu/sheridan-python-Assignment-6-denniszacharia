3796aed7bf31a4f312dd428bd922247a52b8010e
Dennis Zacharia <dennyzach15@gmail.com>
2026-03-23 13:41:06 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,28 +1,40 @@
 class Fibonacci:
     def __init__(self, n):
+        # Validate input
         if not isinstance(n, int):
-            raise ValueError
+            raise ValueError("Input must be an integer")
 
         self.n = n
-        self.index = 0
-        self.a = 0
-        self.b = 1
+        self.current = 0
+        self.next_val = 1
+        self.count = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.index > self.n:
+        # Handle negative case
+        if self.n < 0:
             raise StopIteration
 
-        if self.index == 0:
-            self.index += 1
+        # Stop when done
+        if self.count > self.n:
+            raise StopIteration
+
+        # First value
+        if self.count == 0:
+            self.count += 1
             return 0
 
-        if self.index == 1:
-            self.index += 1
+        # Second value
+        elif self.count == 1:
+            self.count += 1
             return 1
 
-        self.a, self.b = self.b, self.a + self.b
-        self.index += 1
-        return self.b
+        # Fibonacci logic
+        else:
+            value = self.current + self.next_val
+            self.current = self.next_val
+            self.next_val = value
+            self.count += 1
+            return value
