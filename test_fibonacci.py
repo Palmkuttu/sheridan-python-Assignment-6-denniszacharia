08888df16df0e047efa5a6454cdc753e4679af9a
Dennis Zacharia <dennyzach15@gmail.com>
2026-03-23 13:30:47 -0400

    Create test_fibonacci.py

---
+++ test_fibonacci.py
@@ -0,0 +1,25 @@
+from fibonacci import Fibonacci
+
+
+def test_zero():
+    assert list(Fibonacci(0)) == [0]
+
+
+def test_one():
+    assert list(Fibonacci(1)) == [0, 1]
+
+
+def test_two():
+    assert list(Fibonacci(2)) == [0, 1, 1]
+
+
+def test_four():
+    assert list(Fibonacci(4)) == [0, 1, 1, 2, 3]
+
+
+def test_ten():
+    assert list(Fibonacci(10)) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+def test_negative():
+    assert list(Fibonacci(-1)) == []
